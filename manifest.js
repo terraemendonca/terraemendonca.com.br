2f6e917cce367207186b921a933ce5c848d4510f
vshxp <matheus@matfor.com.br>
2024-01-15 13:21:29 -0300

    adicionado manifest

---
+++ manifest.js
@@ -0,0 +1,16 @@
+{
+    "name": "Terra & Mendonca",
+    "short_name": "Terra & Mendonca",
+    "start_url": "https://www.terraemendonca.com.br",
+    "display": "standalone",
+    "background_color": "#fff",
+    "description": "Escritorio de advocacia",
+    "icons": [
+      {
+        "src": "assets/img/logo_no_bg_icon.png",
+        "sizes": "48x48",
+        "type": "image/png"
+      }
+    ],
+    "scope": "https://www.terraemendonca.com.br"
+}
